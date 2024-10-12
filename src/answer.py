2c3460ed1876124eb5cc02eea77b9d0c4cf4c57e
Sahil Ansari <58244066+imsahilansarii@users.noreply.github.com>
2024-10-12 23:12:19 +0530

    Create answer.py
    Signed-off-by: Sahil Ansari <58244066+imsahilansarii@users.noreply.github.com>

---
+++ src/answer.py
@@ -0,0 +1,16 @@
+{
+    "youtube": {
+        "Watch YouTube Video #1": "070624",
+        "Major Games #1": "070624",
+        "Watch YouTube Shorts #3": "070624",
+        "Watch YouTube Shorts #4": "159390",
+        "Watch YouTube Shorts #6": "241086"
+    },
+    "answer": {
+        "choice_1": 10,
+        "choice_2": 14,
+        "choice_3": 12,
+        "choice_4": 1
+    },
+    "expires": 1728777599
+}
